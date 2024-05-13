f43fb2397b6567c66fa409755789a3e4f5071553
wsnaxwhlq1 <wsnaxw@github.com>
2024-05-13 16:51:06 +0800

    pageUpdate

--- static/js/pg/cus/detail.js
+++ static/js/pg/cus/detail.js
@@ -8,6 +8,34 @@ $(document).ready(function(){
     console.log('customerId:'+customerId)
     //默认进行分页数据查询
 
+    
+    const buttons = document.querySelectorAll('.card-body .btn');
+
+    // 为每个按钮添加点击事件监听器
+    buttons.forEach(button => {
+      button.addEventListener('click', function() {
+        // 移除所有按钮的active类
+        buttons.forEach(btn => btn.classList.remove('active'));
+        // 给当前点击的按钮添加active类
+        this.classList.add('active');
+      });
+    });
+
+
+
+    $('#rxxxdiv').hide()
+    
+    $('#movediv').hide()
+    
+    $('#filediv').hide()
+    
+    $('#jobdjv').hide()
+
+
+    $('#kzxxdiv').show()
+
+
+
 
 })
 
@@ -34,4 +62,111 @@ function getParameterByName(name) {
    if (!results) return null;
    if (!results[2]) return '';
    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+
+
+function kzxx(){
+
+
+    
+
+    $('#rxxxdiv').hide()
+    
+    $('#movediv').hide()
+    
+    $('#filediv').hide()
+    
+    $('#jobdjv').hide()
+
+
+    $('#kzxxdiv').show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function rxxx(){
+
+
+
+
+    
+    $('#movediv').hide()
+    
+    $('#filediv').hide()
+    
+    $('#jobdjv').hide()
+
+
+    $('#kzxxdiv').hide()
+
+    $('#rxxxdiv').show()
+
+
+
+
+
+}
+
+function file(){
+
+
+
+    $('#rxxxdiv').hide()
+    
+    $('#movediv').hide()
+    
+    
+    $('#jobdjv').hide()
+
+
+    $('#kzxxdiv').hide()
+    $('#filediv').show()
+
+}
+
+function job(){
+
+
+    $('#rxxxdiv').hide()
+    
+    $('#movediv').hide()
+    
+    $('#filediv').hide()
+    
+
+
+    $('#kzxxdiv').hide()
+    
+    $('#jobdjv').show()
+}
+
+
+function move(){
+
+
+    $('#rxxxdiv').hide()
+    
+    
+    $('#filediv').hide()
+    
+
+
+    $('#kzxxdiv').hide()
+    
+    $('#jobdjv').hide()
+
+    $('#movediv').show()
 }
